f0018d2073a5887fd6d2b2619307c62c56164cd2
TheWeirdDee <divinenation1@gmail.com>
2026-05-11 11:33:21 +0100

    feat: add contracts/hardhat.config.ts

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,36 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 42220,
+    },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 44787,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.CELOSCAN_API_KEY || "",
+    customChains: [
+      {
+        network: "celo",
+        chainId: 42220,
+        urls: {
+          apiURL: "https://api.celoscan.io/api",
+          browserURL: "https://celoscan.io",
+        },
+      },
+    ],
+  },
+};
+
+export default config;

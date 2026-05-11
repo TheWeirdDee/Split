f28fd80514af496be2c4fbb6df989990e4943c9e
TheWeirdDee <divinenation1@gmail.com>
2026-05-11 11:33:27 +0100

    feat: add src/constants/chains.ts

---
+++ src/constants/chains.ts
@@ -0,0 +1,32 @@
+import { defineChain } from 'viem';
+
+export const celo = defineChain({
+  id: 42220,
+  name: 'Celo',
+  network: 'celo',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'CELO',
+    symbol: 'CELO',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://forno.celo.org'],
+    },
+    public: {
+      http: ['https://forno.celo.org'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'CeloScan',
+      url: 'https://celoscan.io',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 13112427,
+    },
+  },
+});
